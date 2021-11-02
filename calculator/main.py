11960998943fef331b1baeb7c4df0e82338df458
isiahdaubon <dauboni4@gmail.com>
2021-11-02 10:03:34 -0400

    Updated the variables in main.py

--- calculator/main.py
+++ calculator/main.py
@@ -1,25 +1,24 @@
 """ This is the increment function"""
 class Calculator:
     """ This is the Calculator class"""
+    History = []
 
-    result = 0
-    def get_result(self):
-        """ Get Result of Calculation"""
-        return self.result
-
-    def add_number(self, value_a):
+    @staticmethod
+    def add_number(value_a, value_b):
         """ adds number to result"""
-        self.result = self.result + value_a
-        return self.result
-    def subtract_number(self, value_a):
+        return value_a + value_b
+
+    @staticmethod
+    def subtract_number(value_a, value_b):
         """ subtract number from result"""
-        self.result = self.result - value_a
-        return self.result
-    def multiply_numbers(self, value_a, value_b):
+        return value_a - value_b
+
+    @staticmethod
+    def multiply_numbers(value_a, value_b):
         """ multiply two numbers and store the result"""
-        self.result = value_a * value_b
-        return self.result
-    def divide_numbers(self, value_a, value_b):
+        return value_a * value_b
+
+    @staticmethod
+    def divide_numbers(value_a, value_b):
         """divide two numbers and store the result"""
-        self.result = value_a / value_b
-        return self.result
+        return  value_a / value_b
